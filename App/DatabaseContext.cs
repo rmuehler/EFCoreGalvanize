2fa4328cbf9014b38db1da5e36f326b5ca03fed4
Robert Muehler <robertmuehler@gmail.com>
2021-05-24 14:09:53 -0400

    added nuget packages for ORM, created a database context

---
+++ App/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreGalvanize
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=app.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
