a8fa8452ee9f6a21dad1dd47ed4d7eda3507fc7e
Robert Muehler <robertmuehler@gmail.com>
2021-05-24 16:34:27 -0400

    query for most number of courses

--- App/Program.cs
+++ App/Program.cs
@@ -15,29 +15,31 @@ namespace EFCoreGalvanize
             database.Students.ToList().ForEach(student => Console.WriteLine(student.FirstName));
 
             //given a student name, show their grades
-            Console.Out.WriteLine("\nEnter a name to search for grades:");
-            string name1 = Console.ReadLine();
+            const string name1 = "Robert";
             var student1 = database.Students.Include(student => student.Grades)
                 .FirstOrDefault(student => student.FirstName.Equals(name1));
-            Console.WriteLine($"{student1.FirstName} has the grades: ");
+            Console.WriteLine($"\n{student1.FirstName} has the grades: ");
             student1.Grades.ToList().ForEach(grade => Console.Write($"{grade.Score} "));
 
             //Average grade of student
-            Console.Out.WriteLine("\nEnter a name to find average grade:");
-            string name2 = Console.ReadLine();
+            const string name2 = "Robert";
             var student2 = database.Students.Include(student => student.Grades)
                 .FirstOrDefault(student => student.FirstName.Equals(name2));
             float average = student2.Grades.Average(grade => grade.Score);
-            Console.Out.WriteLine($"{student2.FirstName}'s average score is {average}.");
+            Console.Out.WriteLine($"\n{student2.FirstName}'s average score is {average}.");
 
             //student with the highest score
             var studentByAveragesDesc = database.Students.Include(student => student.Grades).Select(student => student)
                 .OrderByDescending(student => student.Grades.Average(grade => grade.Score));
             Console.Out.WriteLine(
-                $"\nThe student with the highest average is : {studentByAveragesDesc.First().FirstName}");
+                $"\nThe student with the highest average is: {studentByAveragesDesc.First().FirstName}");
 
             //student with the highest # of courses
-            
+            var studentByNumCoursesDesc = database.Students.Include(student => student.Grades)
+                .Select(student => student)
+                .OrderByDescending(student => student.Grades.Count);
+            Console.Out.WriteLine(
+                $"\nThe student with the most number of courses is: {studentByNumCoursesDesc.First().FirstName}");
         }
     }
 }
