b29c13c25deeaad27205a26e1ea9c59644b9e51a
Robert Muehler <robertmuehler@gmail.com>
2021-05-24 14:02:15 -0400

    created Grade model

---
+++ App/Grade.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreGalvanize
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string CourseName { get; set; }
+        public float Score { get; set; } //cannot be name of Class, assume this is Grade
+    }
+}
