cfb6b03523567065cd89f36df6bcb72c8156988d
Robert Muehler <robertmuehler@gmail.com>
2021-05-24 15:16:23 -0400

    print list of students from database

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreGalvanize
 {
@@ -6,7 +8,20 @@ namespace EFCoreGalvanize
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DatabaseContext database = new DatabaseContext();
+            
+            //write list of student names
+            database.Students.ToList().ForEach(student => Console.WriteLine(student.FirstName));
+            
+            //given a student name, show their grades
+            
+            // Console.Out.WriteLine("Enter a name to search for grades:");
+            // string name = Console.ReadLine();
+            // var student1 = database.Students.FirstOrDefault(student => student.FirstName.Equals(name));
+            // Console.WriteLine($"{student1} has the grades: ");
+            // var grades = student1.Grades.ToList();
+            
+            
         }
     }
 }
