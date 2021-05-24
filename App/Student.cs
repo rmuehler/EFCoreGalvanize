80d4c6c617a0713cc281e36239eb146e7b515bae
Robert Muehler <robertmuehler@gmail.com>
2021-05-24 13:59:23 -0400

    set up Student model

---
+++ App/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreGalvanize
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Class Classification { get; set; }
+        public List<Grade> Grades { get; set; }
+
+        public enum Class
+        {
+            Freshman,
+            Sophomore,
+            Junior,
+            Senior
+        }
+    }
+}
