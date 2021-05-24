7394fc2e15fe4e6acb962a2b4783a68d6d9c4d4a
Robert Muehler <robertmuehler@gmail.com>
2021-05-24 15:58:12 -0400

    find average by student

---
+++ App/Migrations/20210524192803_Removed Key Field.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreGalvanize.Migrations
+{
+    public partial class RemovedKeyField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,33 +1,37 @@
 ﻿using System;
+using System.Data;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreGalvanize
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            DatabaseContext database = new DatabaseContext();
-            
+            var database = new DatabaseContext();
+
             //write list of student names
             database.Students.ToList().ForEach(student => Console.WriteLine(student.FirstName));
-            
+
             //given a student name, show their grades
-            
             Console.Out.WriteLine("\nEnter a name to search for grades:");
-            string name = Console.ReadLine();
-            var student1 = database.Students.Include(student => student.Grades ).FirstOrDefault(student => student.FirstName.Equals(name));
+            string name1 = Console.ReadLine();
+            var student1 = database.Students.Include(student => student.Grades)
+                .FirstOrDefault(student => student.FirstName.Equals(name1));
             Console.WriteLine($"{student1.FirstName} has the grades: ");
-            student1.Grades.ToList().ForEach(grade => Console.Write($"{grade.Score}" ));
+            student1.Grades.ToList().ForEach(grade => Console.Write($"{grade.Score} "));
 
-            // const string student2Name = "Robert";
-            // var student2 = database.Students.FirstOrDefault(student => student.FirstName.Equals(student2Name));
-            // // var average = student2.Grades.ToList().Average();
+            //Average grade of student
+            Console.Out.WriteLine("\nEnter a name to find average grade:");
+            string name2 = Console.ReadLine();
+            var student2 = database.Students.Include(student => student.Grades)
+                .FirstOrDefault(student => student.FirstName.Equals(name2));
+            float average = student2.Grades.Average(grade => grade.Score);
+            Console.Out.WriteLine($"{student2.FirstName}'s average score is {average}.");
 
-            //var averageGrades = database.Students.
-
-
+             
+            
         }
     }
 }
